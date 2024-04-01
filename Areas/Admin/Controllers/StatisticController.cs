264c1921e8137b3b39f341471b05ef7708d16a29
maniacpg <quazkd@gmail.com>
2024-04-01 22:42:08 +0700

    add statistic

---
+++ Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WatchShop2.Models;
+
+namespace WatchShop2.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/statistic")]
+    public class StatisticController : Controller
+    {
+        
+        private WatchShop2Context _entityContext { get; }
+
+
+        public StatisticController(WatchShop2Context entityContext)
+        {
+            _entityContext = entityContext;
+        }
+        public IActionResult Index()
+        {
+            // Thống kê số lượng sản phẩm trong từng danh mục
+            var productCountByCategory = _entityContext.Categories
+                .Select(c => new { CategoryName = c.CategoryName, ProductCount = c.Products.Count })
+                .ToList();
+
+            ViewBag.ProductCountByCategory = productCountByCategory;
+
+            // Thống kê số lượng đơn hàng theo trạng thái
+            var orderCountByStatus = _entityContext.Orders
+                .GroupBy(o => o.Status)
+                .Select(g => new { Status = g.Key, OrderCount = g.Count() })
+                .ToList();
+
+            ViewBag.OrderCountByStatus = orderCountByStatus;
+
+            return View();
+        }        
+
+    }
+}
--- obj/Debug/net8.0/WatchShop2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WatchShop2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WatchShop2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282e23111a35ccc692453dca96d9263a0d0e3775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03d1484f940cf86ecfe81657cda618920ebff90")]
 [assembly: System.Reflection.AssemblyProductAttribute("WatchShop2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WatchShop2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
